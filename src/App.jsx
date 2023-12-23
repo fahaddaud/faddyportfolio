5698ba53f89f0be825745e875d7cf9aaf8c590cd
fahaddaud <57704392+fahaddaud@users.noreply.github.com>
2023-12-23 09:23:14 +0000

    set homepage

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Fahad Daud",
+  title: "Web Designer & Developer",
+  email: "fahaddaud324@gmail.com",
+  gitHub: "fahad Daud",
+  instagram: "fahad._.daud",
+  linkedIn: "Fahad Daud",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "faddydas",
+  youTube: "faddy",
 };
 
 const primaryColor = "#4E567E";
